65601d5acfd0f2443a5f7c5c15d4f5e022cd690b
Nenad Raic <raic.nenad@gmail.com>
2025-12-01 13:10:49 +0100

    prvi dio zadatka

--- Armstrong2/src/Main.java
+++ Armstrong2/src/Main.java
@@ -1,9 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
-        System.out.println("Hello and welcome1!");
 
         // prolazimo kroz sve brojeve od 1 do 9999
         for (int broj = 1; broj <= 9999; broj++) {
