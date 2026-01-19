134e5eff5f8fd86f2edff81d7e641970f96c361f
Nenad Raic <raic.nenad@gmail.com>
2026-01-19 14:37:45 +0100

    2.1

---
+++ drugi_zadatak_2_1/src/BrojacZnakova.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class BrojacZnakova {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Unesite string: ");
+        String s = sc.nextLine();
+
+        int slova = 0;
+        int brojevi = 0;
+        int ostali = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (Character.isLetter(c)) {
+                slova++;
+            } else if (Character.isDigit(c)) {
+                brojevi++;
+            } else {
+                ostali++;
+            }
+        }
+
+        System.out.printf("Slova: %d%n", slova);
+        System.out.printf("Brojevi: %d%n", brojevi);
+        System.out.printf("Ostali znakovi: %d%n", ostali);
+    }
+}
